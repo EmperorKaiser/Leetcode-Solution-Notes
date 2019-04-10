0ba794f6f2c4ddbb0b9d62e93e903abe4c4af66c
EmperorKaiser <Kaiser_Ou@163.com>
2019-04-10 11:48:29 +0800

    Create 009_Palindrome Number.py

---
+++ 009_Palindrome Number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        '''
+        Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+        '''
+        # My function starts here
+        xList=list(str(x))
+        nLoop=math.floor(len(xList)/2)
+        for i in range(nLoop):
+            if xList[i]!=xList[-i-1]:
+                return False
+        return True
+        '''
+        abstract
+        Using list and compare the string of x.Mention that the sign is also turned into a char in the String
+        '''
